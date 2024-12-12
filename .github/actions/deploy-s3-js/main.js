bfb6d13314cf6ee083b5c4aa39ece1e920a09e30
Ehudaviv <ehudaviv@outlook.com>
2024-12-12 12:00:52 +0200

    output website url

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -11,6 +11,9 @@ function run() {
 
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
+
+  const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
