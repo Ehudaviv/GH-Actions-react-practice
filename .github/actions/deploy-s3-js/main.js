80ccf0c3a2c1ceeeb39d67a11b5114aa644afae1
Ehudaviv <ehudaviv@outlook.com>
2024-12-12 11:51:47 +0200

    js upload site to s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,16 @@
-const core = require('@actions/core');
-const exec  = require('@actions/exec');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const github = require("@actions/github");
 
 function run() {
-  core.notice('Deploying to S3');
+  core.notice("Deploying to S3");
+
+  const bucket = core.getInput("bucket");
+  const distFolder = core.getInput("dist-folder");
+  const region = core.getInput("region");
+
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
 }
 
 run();
